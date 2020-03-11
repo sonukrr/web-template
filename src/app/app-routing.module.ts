5d8fe7429ef6c1773d3f620aa280e43253781a3c
Sonu Kumar <sonuec12@gmail.com>
2020-03-11 21:45:38 +0530

    landing page with routing links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { LockComponent } from './components/pages/lock/lock.component';
 
+const routes: Routes = [
+  {
+    path: 'landing',
+    children: [
+      {
+        path: '',
+        component: LandingPageComponent,
+        pathMatch: 'full'
 
-const routes: Routes = [];
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        pathMatch: 'full'
+      },
+      {
+        path: 'lock',
+        component: LockComponent,
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { LoginComponent } from './components/pages/login/login.component';
+import { LockComponent } from './components/pages/lock/lock.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent,
+    LockComponent
   ],
   imports: [
     BrowserModule,
