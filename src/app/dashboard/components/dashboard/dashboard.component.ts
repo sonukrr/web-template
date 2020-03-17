a26d0f6c786ee1b2d0c66d0fe81e5165186bf4c4
Sonu Kumar <sonuec12@gmail.com>
2020-03-18 01:05:42 +0530

    completed sidebar nav on dashboard

--- src/app/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
   constructor() {
     
    }
-
+ 
   ngOnInit() {
-    console.log("dashboard component");
     
   }
+
+
+
+
+  
 
 }
