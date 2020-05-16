da33aa6720ad23fd86f110d533f6cdc04dfa9f7c
Sonu Kumar <sonu.kumar@zs.com>
2020-05-16 20:12:51 +0530

    heroku app config fixed

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/MuseUI'));
+app.use(express.static(__dirname + '/dist/web-template'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/MuseUI/index.html'));
+res.sendFile(path.join(__dirname+'/dist/web-template/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
